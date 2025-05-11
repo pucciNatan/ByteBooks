46b9c0190a53c8a246d4768fde768acf8854d44e
Natan <natan.puccib@outlook.com>
2025-05-11 18:44:35 -0300

    gerenciando URL padrao

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from django.urls import path
 from .api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
+    path("", RedirectView.as_view(url="/admin/", permanent=False)),
 ]
 
 
