dc4ef773fec19a7652aa94219ca05a4461e4c858
Natan <natan.puccib@outlook.com>
2025-05-11 19:49:59 -0300

    deployyy

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,11 +4,17 @@ from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 from django.urls import path
 from .api import api
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
     path("", RedirectView.as_view(url="/admin/", permanent=False)),
+    re_path(r"^media/(?P<path>.*)$",
+            serve,
+            {"document_root": settings.MEDIA_ROOT,
+             "show_indexes": False}),
 ]
 
 
